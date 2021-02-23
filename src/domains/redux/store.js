89acb56a2b5245c810670288d9d6791775b749c8
Blancduman <blancduman@gmail.com>
2021-02-23 17:53:22 +0300

    fix: redux dev tools

--- src/domains/redux/store.js
+++ src/domains/redux/store.js
@@ -2,10 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-
 export default function configureStore(initialState) {
-  const enhancer = composeEnhancers(applyMiddleware(thunk));
+  const enhancer = applyMiddleware(thunk);
 
   return createStore(rootReducer, initialState, enhancer);
 }
